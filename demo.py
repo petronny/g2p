ef6b29f988d49ffd68520a55d4487989ce8a052f
Jingbei Li <lijingbei@100tal.com>
2019-04-26 18:02:36 +0800

    updated demo.py

--- demo.py
+++ demo.py
@@ -1,17 +1,17 @@
 from en_gb import G2P
 
 g2p = G2P()
-print(g2p['hello'])
-print(g2p['google'])
+print(g2p.convert('hello'))
+print(g2p.convert('github'))
 
 from en_us import G2P
 
 g2p = G2P()
-print(g2p['hello'])
-print(g2p['google'])
+print(g2p.convert('hello'))
+print(g2p.convert('github'))
 
 from zh_cn import G2P
 
 g2p = G2P()
-print(g2p['好吧'])
-print(g2p['是我'])
+print(g2p.convert('今天天气不错'))
+print(g2p.convert('好吧', False))
